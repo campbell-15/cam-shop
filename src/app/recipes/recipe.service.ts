dfc3a07d29b7e6633a25215bb9a04fecfffc2f85
Campbell Davies-Webb <campbelldavies-webb@Campbells-MacBook-Air.local>
2023-07-10 21:38:42 +0200

    fixed bugs

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,38 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-recipeSelected = new EventEmitter<Recipe>();
+    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Tasty Pancake', 
-        'This is simply the best breakfast!', 
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDWuWCkEMPQMlzAyPRNm_uKzuNM2tukraxxIZRVxzge8SfLIn3G46o4kQnlVOeGbB42Bw&usqp=CAU',
-        [
-            new Ingredient('Flour', 1),
-            new Ingredient('Milk', 1),
-            new Ingredient('Blueberries', 1)
-        ])
+    new Recipe(
+    'Tasty Schnitzel',
+    'A super-tasty Schnitzel - just awesome!',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3DJt6fbM1D4WEBRivltjD8bb-7DpYxtbFQA&usqp=CAU',
+    [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Big Juicy Burger',
+    'What else needs to be said?',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSko_1kWLwbFom_uqDIDO0w6XgOtA4zyxcj4u10vFSrl_dz1_b6TaMsNAhlpAAlZJe7_Jk&usqp=CAU',
+    [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+    ])
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
