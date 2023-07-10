1a00999e4d98b6cb4f32e73f73691c60fadca033
Campbell Davies-Webb <campbelldavies-webb@Campbells-MacBook-Air.local>
2023-07-10 18:10:04 +0200

    interpolation

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,19 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDWuWCkEMPQMlzAyPRNm_uKzuNM2tukraxxIZRVxzge8SfLIn3G46o4kQnlVOeGbB42Bw&usqp=CAU')
+        new Recipe('A Tasty Pancake', 
+        'This is simply the best breakfast!', 
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDWuWCkEMPQMlzAyPRNm_uKzuNM2tukraxxIZRVxzge8SfLIn3G46o4kQnlVOeGbB42Bw&usqp=CAU',
+        [
+            new Ingredient('Flour', 1),
+            new Ingredient('Milk', 1),
+            new Ingredient('Blueberries', 1)
+        ])
     ];
 
     getRecipes() {
