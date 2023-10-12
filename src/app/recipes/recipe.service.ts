bbbd5297d78be7f93db65e6c0a4d7d2180b88ae5
campbell-15 <campbelldavieswebb@gmail.com>
2023-10-12 15:51:21 +0200

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,23 +9,24 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-    new Recipe(
-    'Tasty Schnitzel',
-    'A super-tasty Schnitzel - just awesome!',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3DJt6fbM1D4WEBRivltjD8bb-7DpYxtbFQA&usqp=CAU',
-    [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-    ]),
-    new Recipe('Big Juicy Burger',
-    'What else needs to be said?',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSko_1kWLwbFom_uqDIDO0w6XgOtA4zyxcj4u10vFSrl_dz1_b6TaMsNAhlpAAlZJe7_Jk&usqp=CAU',
-    [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-    ])
-    ];
+    // private recipes: Recipe[] = [
+    // new Recipe(
+    // 'Tasty Schnitzel',
+    // 'A super-tasty Schnitzel - just awesome!',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3DJt6fbM1D4WEBRivltjD8bb-7DpYxtbFQA&usqp=CAU',
+    // [
+    //     new Ingredient('Meat', 1),
+    //     new Ingredient('French Fries', 20)
+    // ]),
+    // new Recipe('Big Juicy Burger',
+    // 'What else needs to be said?',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSko_1kWLwbFom_uqDIDO0w6XgOtA4zyxcj4u10vFSrl_dz1_b6TaMsNAhlpAAlZJe7_Jk&usqp=CAU',
+    // [
+    //     new Ingredient('Buns', 2),
+    //     new Ingredient('Meat', 1)
+    // ])
+    // ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
 
